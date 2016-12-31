e6c30edae45b4c01b428c923932f1b3ff291e875
adam <lukacs.adam@flowtex.hu>
2016-12-31 05:12:56 +0100

    kk

--- pictures/slideshow2.sh
+++ pictures/slideshow2.sh
@@ -5,17 +5,25 @@ IMG_PATH=`pwd`"/images/"
 
 cd $IMG_PATH
 
-# File list. Can either be hard-coded, space separated,
-# or to use the output of a command, use $(command). The
-# safest approach is to use a shell glob as I have done here.
-FILE_LIST=*.txt
 
 # How long to run the command in seconds before killing it?
 TIMEOUT=1
 
-# For every file on the list...
-for FILE in $FILE_LIST
+while true
 do
+
+# File list. Can either be hard-coded, space separated,
+# or to use the output of a command, use $(command). The
+# safest approach is to use a shell glob as I have done here.
+FILE_LIST=*.jpg
+
+
+FILES_COUNT=`ls -1 *.jpg | wc -l`
+
+
+# For every file on the list...
+#for FILE in $FILE_LIST
+#do
   # OK, this is where the magic happens...
   # First invoke a shell and feed the mini script (in double quotes)
   # to that shell. The mini script first executes fbi with whatever
@@ -32,5 +40,9 @@ do
   # Also you may want to try removing the -9 parameter to kill in
   # case it messes up your framebuffer... not sure how well fbi
   # handles a forceful (-9) vs. graceful (no -9) kill.
-  sh -c "(sleep $TIMEOUT; kill -9 \$\$) & exec fbi $FILE"
+  killall -9 fbi
+  fbi -T 2 -noverbose -u -a -t $TIMEOUT $FILE_LIST 
+  sleep $(($TIMEOUT*$FILES_COUNT))
+#done
+
 done
