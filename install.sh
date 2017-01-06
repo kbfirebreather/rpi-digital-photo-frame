d3e55f148b30c6678e777745dddbdeafbbfd8954
bsvdoom <flowtex@gmail.com>
2017-01-06 13:51:35 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,46 +1,49 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install fbi imagemagick
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+apt-get update && apt-get install fbi imagemagick
 
 cd pictures
 pwd > homedir.txt
-sudo chmod +x *.sh
+chmod +x *.sh
 mkdir images
-cd ..
 
+cat <<EOT >> /etc/init.d/slideshow
+#! /bin/sh
+### BEGIN INIT INFO
+# Provides:          slideshow
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable service provided by daemon.
+### END INIT INFO
+case "$1" in
+    start)
+        echo "Starting slideshow"
+        cd `pwd`
+        ./slideshow.sh
+    ;;
+    stop)
+        echo -n "Shutting down noip2"
+        for i in `ps aux | grep './slideshow.sh' | grep -v grep | awk '{print $2}'`
+        do
+          kill -9 $i
+        done
+    ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+        exit 1
+esac
+exit 0
+EOT
 
-sudo echo "#! /bin/sh \
-### BEGIN INIT INFO \
-# Provides:          slideshow \
-# Required-Start:    $remote_fs $syslog \
-# Required-Stop:     $remote_fs $syslog \
-# Default-Start:     2 3 4 5 \
-# Default-Stop:      0 1 6 \
-# Short-Description: Start daemon at boot time \
-# Description:       Enable service provided by daemon. \
-### END INIT INFO \
-case "$1" in \
-    start) \
-        echo "Starting slideshow" \
-        cd `pwd` \
-        ./slideshow.sh \
-    ;; \
-    stop) \
-        echo -n "Shutting down noip2" \
-        for i in `ps aux | grep './slideshow.sh' | grep -v grep | awk '{print $2}'` \
-        do \
-          kill -9 $i \
-        done \
-    ;; \
-    *) \
-        echo "Usage: $0 {start|stop}" \
-        exit 1 \
-esac \
-exit 0 \
-" > /etc/init.d
+chmod +x /etc/init.d/slideshow
+update-rc.d slideshow defaults 91
 
-
-sudo chmod +x /etc/init.d/slideshow
-sudo update-rc.d slideshow defaults 91
-
-echo "Done."
+echo "\n\tDone.\n"
