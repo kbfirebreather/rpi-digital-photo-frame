f6ad521c8838c235cfca221dd860bf06a95bbdf8
bsvdoom <flowtex@gmail.com>
2017-01-18 18:04:55 +0100

    Update rotate.sh

--- pictures/rotate.sh
+++ pictures/rotate.sh
@@ -9,8 +9,10 @@ FILE_LIST=`ls -1 *.jpg`
 
 for i in $FILE_LIST
 do
-if [ $i =~ _rotated.jpg$ ]; then
+if [[ $i =~ _rotated.jpg$ ]]; then
 	NEWNAME=$( $i | cut -f 1 -d '.' )
 	convert $i -rotate 180 $NEWNAME_rotated.jpg
+else
+	echo "nothing to do";
 fi
 done
